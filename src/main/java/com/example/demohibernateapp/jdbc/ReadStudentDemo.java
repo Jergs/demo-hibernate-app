60d3182f663e7afb0c20ad023a9da97b12d838c5
Yehor Havryliuk <yehor.havryliuk@gmail.com>
2022-08-12 19:39:29 +0300

    Added update

--- src/main/java/com/example/demohibernateapp/jdbc/ReadStudentDemo.java
+++ src/main/java/com/example/demohibernateapp/jdbc/ReadStudentDemo.java
@@ -24,10 +24,19 @@ public class ReadStudentDemo {
             System.out.println("Student: " + student.getFirstName() + " " + student.getLastName());
 
             // HQL example
-            List<Student> theStudents =  session.createQuery("from Student s where s.lastName='Wall'" +
+            List<Student> theStudents = session.createQuery("from Student s where s.lastName='Wall'" +
                     "OR s.firstName='Egor' OR s.email LIKE '%luv2code.com'").getResultList();
 
             theStudents.forEach(System.out::println);
+
+            // Update students example
+            theStudents.forEach(st -> {
+                if (st.getFirstName().equals("Egor"))
+                    st.setEmail("gavrilyk2903@gmail.com");
+            });
+
+            // Update all students emails
+            //session.createQuery("update Student set email='foo@gmail.com'").executeUpdate();
 
             session.getTransaction().commit();
 
